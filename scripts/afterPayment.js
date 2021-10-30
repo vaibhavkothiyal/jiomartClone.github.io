0de6bcd960e4c5854326c5d899a35a798d76c22c
arpit1024 <91615308+arpit1024@users.noreply.github.com>
2021-10-30 22:04:19 +0530

    Delete afterPayment.js

--- scripts/afterPayment.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("orderNo").innerText=randNum();
-    document.getElementById("orderNo").style.color="green";
-    function randNum(){
-            return Math.floor((Math.random() * 249037) + 189654);
-        }
-    function gotohome(){
-        window.location.href="home.html";
-    }    
